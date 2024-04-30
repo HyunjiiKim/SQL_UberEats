4c8c16ecd6e316f088ad24cfb553fb63353a01fc
Hyunji <hjkim96727@gmail.com>
2024-05-01 01:16:53 +0200

    first commit

---
+++ DB_UberEats.sql
@@ -0,0 +1,19 @@
+CREATE DATABASE UberEats;
+
+CREATE TABLE client(
+    id varchar(127);
+    pw varchar(20);
+    
+)
+
+CREATE TABLE order(
+    id int();
+)
+
+CREATE TABLE restaurant(
+
+);
+
+CREATE TABLE delivery(
+
+);
